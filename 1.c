38e6ac3ae77b76a265dbec66f6c223a628d17ebf
asifisa <49895737+asifisa@users.noreply.github.com>
2019-06-11 12:18:17 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100];
+    scanf("%s",s);
+    if(s[0]=='S')
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+    return 0;
+}
